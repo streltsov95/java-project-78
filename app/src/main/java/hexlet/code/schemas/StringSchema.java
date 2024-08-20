13ff4b2e53301740be2bb2202b492af44d4b7e86
Viktor Streltsov <streltsov95va@gmail.com>
2024-08-20 21:49:29 +0300

    add StringSchema.java

---
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -0,0 +1,41 @@
+package hexlet.code.schemas;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Predicate;
+
+public class StringSchema {
+
+    private Map<String, Predicate<String>> rules;
+
+    public StringSchema() {
+        rules = new LinkedHashMap<>();
+    }
+
+    public StringSchema required() {
+        Predicate<String> isRequired = str -> str != null && !str.isEmpty();
+        rules.put("REQUIRED", isRequired);
+        return this;
+    }
+
+    public StringSchema minLength(int length) {
+        Predicate<String> isMinLength = str -> str.length() >= length;
+        rules.put("MIN_LENGTH", isMinLength);
+        return this;
+    }
+
+    public StringSchema contains(String sample) {
+        Predicate<String> isContains = str -> str.contains(sample);
+        rules.put("CONTAINS", isContains);
+        return this;
+    }
+
+    public boolean isValid(String data) {
+        for (var rule : rules.values()) {
+            if (!rule.test(data)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
