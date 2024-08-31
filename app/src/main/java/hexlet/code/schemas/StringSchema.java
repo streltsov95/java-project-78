a69c2084bbe2f62072db80ab087dfc47850a77f4
Viktor Streltsov <streltsov95va@gmail.com>
2024-08-31 18:21:35 +0300

    fix: add check for null

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -12,13 +12,15 @@ public final class StringSchema extends BaseSchema<String> {
     }
 
     public StringSchema minLength(int length) {
-        Predicate<String> isMinLength = str -> str.length() >= length;
+        Predicate<String> isMinLength = str -> isRequired
+                ? str != null && str.length() >= length : str == null || str.length() >= length;
         rules.put("MIN_LENGTH", isMinLength);
         return this;
     }
 
     public StringSchema contains(String sample) {
-        Predicate<String> isContains = str -> str.contains(sample);
+        Predicate<String> isContains = str -> isRequired
+                ? str != null && str.contains(sample) : str == null || str.contains(sample);
         rules.put("CONTAINS", isContains);
         return this;
     }
